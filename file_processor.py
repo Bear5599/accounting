049cf5616b376b92b5d41018ca5800ae4ea02658
Jonbd3 <jonbd3@gmail.com>
2024-01-24 13:48:36 -0500

    making this app modular

---
+++ file_processor.py
@@ -0,0 +1,19 @@
+class FileProcesser:
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
+        self.files = []
+
+    def get_files(self):
+        self.files = os.listdir(self.folder_path)
+
+    def full_path(self):
+        # Generate a list of full file paths
+        full_paths = []
+        for file in self.files:
+            full_paths.append(os.path.join(self.folder_path, file))
+        return full_paths
+
+
+file_processer = FileProcesser(accounting_files_dir)
+file_processer.get_files()
+print(file_processer.files)
