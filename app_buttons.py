e125f21d36fd2352d5e6aa4de1e5e9b0489555c9
Jonbd3 <jonbd3@gmail.com>
2024-01-24 17:12:58 -0500

    code structure

--- app_buttons.py
+++ app_buttons.py
@@ -1,17 +1,20 @@
 import tkinter as tk
-import file_processor
+from file_processor import FileProcesser
+import os
+
+folder_path = os.path.expanduser("~/Documents/")
 
 class FileProcessorButtons:
-    def __init__(self):
+    def __init__(self, button_maker):
         self.root = tk.Tk()
         self.root.title("File Processor")
+        self.button_maker = button_maker
 
         # Example of adding a button
         self.process_button = tk.Button(self.root, text="Process File", command=self.process_file)
         self.process_button.pack()
 
-    def process_file(self):
-        
+    def button_creator(self):
         pass
 
     def run(self):
--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-# program for converting bills into searchable files for excell
-import os
-
+from file_processor import FileProcesser
+from app_buttons import FileProcessorButtons
 
 class FileMaker:
-    def __init__(self):
-        pass
-        
+    def __init__(self, folder_path):
+        self.file_processor = FileProcesser(folder_path)
+        self.ui = FileProcessorButtons(self.file_processor)
+
+    def run(self):
+        self.ui.run()
 
 if __name__ == "__main__":
-    accounting_files_dir = os.path.expanduser("~/Documents/accounting_files")
-
-    main_app = FileMaker()
-    main_app()
+    pass
