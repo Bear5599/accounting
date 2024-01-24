70048ced7a224661d95c9e398f69b79325ab4f04
Jonbd3 <jonbd3@gmail.com>
2024-01-23 21:36:37 -0500

    adding path reading

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 # program for converting bills into searchable files for excell
 import os
 
-accounting_files = os.path.expanduser("~/Documents/accounting_files")
+accounting_files_dir = os.path.expanduser("~/Documents/accounting_files")
+
+def file_finder(directory):
+    file_names = []
+    for file in os.listdir(directory):
+        full_path = os.path.join(directory, file)
+        if os.path.isfile(full_path):
+            file_names.append(full_path)
+    print(file_names)
+    return file_names
+
