f0525c095495060b1ed01738f6c38aab579acbc0
Jonbd3 <jonbd3@gmail.com>
2024-01-25 13:27:38 -0500

    logic

--- file_processor.py
+++ file_processor.py
@@ -1,6 +1,5 @@
 import os
 from openpyxl import Workbook
-import pandas
 
 accounting_folder = os.path.expanduser("~/Documents/accounting_files")
 
@@ -8,6 +7,7 @@ class FileProcessor:
     def __init__(self, folder_path):
         self.folder_path = folder_path
         self.files = []
+        self.all_files_content = []
 
     def get_files(self):
         self.files = os.listdir(self.folder_path)
@@ -24,8 +24,20 @@ class FileProcessor:
 
         for file in csv_files:
             if file.endswith(".csv"):
-                pass
+                with open(file, "r") as acct_files:
+                    for line in acct_files:
+                        self.all_files_content = line
+                    
+    def search_through_all(self, search):
+        result = []
+        for lines in self.all_files_content:
+            if search in lines:
+                result.append(lines)
+                print(result)
+                return result
+
 
 fp_accnt_files = FileProcessor(accounting_folder)
-
 fp_accnt_files.csv_to_excel()
+user_input = input("Search through the csv files: ")
+fp_accnt_files.search_through_all(user_input)
