a229bdfba61d472d942b3a992951ce32876d664c
Jonbd3 <jonbd3@gmail.com>
2024-01-25 14:51:30 -0500

    testing function

--- file_processor.py
+++ file_processor.py
@@ -10,31 +10,35 @@ class FileProcessor:
         self.all_files_content = []
 
     def get_files(self):
+        # gets the files in a folder
         self.files = os.listdir(self.folder_path)
 
     def full_path(self):
+        # appends the file names to the folder path
         full_paths = []
         for file in self.files:
             full_paths.append(os.path.join(self.folder_path, file))
         return full_paths
 
     def csv_to_excel(self):
+        # checks if files are .csv then combines them into a list
         self.get_files()
         csv_files = self.full_path()
+        
 
         for file in csv_files:
             if file.endswith(".csv"):
                 with open(file, "r") as acct_files:
                     for line in acct_files:
-                        self.all_files_content = line
+                        self.all_files_content.append(line)
                     
     def search_through_all(self, search):
         result = []
         for lines in self.all_files_content:
-            if search in lines:
+            if search.lower() in lines.lower():
                 result.append(lines)
-                print(result)
-                return result
+        print(len(result))
+                
 
 
 fp_accnt_files = FileProcessor(accounting_folder)
---
+++ test.py
@@ -0,0 +1,18 @@
+import os
+
+file_path = os.path.expanduser("~/Documents/accounting_files/wellsfargo_breezy_bills_1-1-23-to-1-23-24.csv")
+user_input = str(input("Enter a search word for the file: "))
+def test_program(folder, search):
+    results = []
+    with open(folder, "r") as file:
+        for lines in file:
+            if search.lower() in lines.lower():
+                results.append(lines)
+    print(f"There are {len(results)} occurances in this file")
+    for items in results:
+        print(items)
+test_program(file_path, user_input)
+
+
+
+
