0b17ff6cabea1de5681b342bd8de6f843396980b
Jonbd3 <jonbd3@gmail.com>
2024-01-27 22:29:19 -0500

    getting csv file names

--- file_processor.py
+++ file_processor.py
@@ -20,8 +20,10 @@ class FileProcessor:
         return self.files
     
     def get_names_without_ext(self):
-        for files in self.folder_path:
-            self.base_file_names.append(os.path.basename(files))
+        self.get_files()
+        for files in self.files:
+            if files.endswith(".csv"):
+                self.base_file_names.append(os.path.basename(files.strip(".csv")))
         return self.base_file_names
 
     def full_path(self):
