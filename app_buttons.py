d9d9bdb3edfe4af57e1fd71c94f4a9708a08e44d
Jonbd3 <jonbd3@gmail.com>
2024-01-28 18:44:26 -0500

    buttons

--- app_buttons.py
+++ app_buttons.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from file_processor import FileProcessor
 import os
+import time
 
 folder_path = os.path.expanduser("~/Documents/accounting_files")
 
@@ -11,6 +12,7 @@ class FileProcessorButtons:
         self.root.title("CSV Converter app")
         self.file_processor = FileProcessor(folder_path)
         self.button_names = self.file_processor.get_names_without_ext()
+        self.first_buttons = {}
 
         # Example of adding a button
         
@@ -18,6 +20,11 @@ class FileProcessorButtons:
         for names in self.button_names:
             csv_buttons = tk.Button(self.root, text=names)
             csv_buttons.pack()
+            self.first_buttons[names] = csv_buttons
+    
+    def hide_first_buttons(self):
+        for buttons in self.first_buttons.values():
+            buttons.pack_forget()
         
 
     def run(self):
