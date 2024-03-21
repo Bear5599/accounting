46a2ecafd27aec4ad040584687c789d241b2cfaf
Jonbd3 <jonbd3@gmail.com>
2024-03-21 18:46:13 -0400

    refactoring

--- pdf_organizer.py
+++ pdf_organizer.py
@@ -1,20 +1,37 @@
 import os
 
+class PdfOrganizer:
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
+        self.files = []
+        self.pdf_files = []
+        self.base_file_names = []
+        self.full_file_paths = []
+        self.get_files()  # Initialize files and related properties
 
-def get_files(self):
-    # gets the files in a folder
-    self.files = os.listdir(self.folder_path)
-    return self.files
-    
-def get_names_without_ext(self):
-    self.get_files()
-    for files in self.files:
-        if files.endswith(".csv"):
-            self.base_file_names.append(os.path.basename(files.strip(".csv")))
-    return self.base_file_names
+    def get_files(self):
+        # Gets the files in a folder and initializes PDF-related lists
+        self.files = os.listdir(self.folder_path)
+        self.filter_pdf_files()
+        self.set_base_file_names()
+        self.set_full_file_paths()
 
-def full_path(self):
-    # appends the file names to the folder path
-    for file in self.files:
-        self.full_file_paths.append(os.path.join(self.folder_path, file))
-    return self.full_file_paths
+    def filter_pdf_files(self):
+        # Filters out PDF files from self.files
+        self.pdf_files = [file for file in self.files if file.endswith('.pdf')]
+
+    def set_base_file_names(self):
+        # Sets base file names for PDF files (without extension)
+        for file in self.pdf_files:
+            base_name = os.path.splitext(file)[0]
+            self.base_file_names.append(base_name)
+
+    def set_full_file_paths(self):
+        # Sets full file paths for PDF files
+        for file in self.pdf_files:
+            full_path = os.path.join(self.folder_path, file)
+            self.full_file_paths.append(full_path)
+
+# Usage
+banking = os.path.expanduser("~/Documents/e_banking")
+my_files = PdfOrganizer(banking)
