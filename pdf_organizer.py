53aa62ecc478aafeec825bc75ef1bb11310155fb
Jonbd3 <jonbd3@gmail.com>
2024-03-23 13:40:34 -0400

    swapped modules

--- pdf_organizer.py
+++ pdf_organizer.py
@@ -1,5 +1,11 @@
 import os
 import PyPDF4
+import tabula
+import warnings
+
+# Suppress specific FutureWarnings
+warnings.filterwarnings("ignore", category=FutureWarning, message=".*errors='ignore' is deprecated.*")
+
 
 class PdfOrganizer:
     def __init__(self, folder_path):
@@ -8,6 +14,7 @@ class PdfOrganizer:
         self.pdf_files = []
         self.base_file_names = []
         self.full_file_paths = []
+        self.pdf_content = []
         self.get_files()  # Initialize files and related properties
 
     def get_files(self):
@@ -33,6 +40,28 @@ class PdfOrganizer:
             full_path = os.path.join(self.folder_path, file)
             self.full_file_paths.append(full_path)
 
+    def pdf_table(self):
+        tables = tabula.read_pdf(self.full_file_paths[1], pages="all")
+        with open("pdf_tables.txt", "a") as pdf_table:
+            for table in tables:
+                pdf_table.write(table.to_csv())  # Convert the DataFrame to string
+                pdf_table.write("\n\n")  # Add some space between tables for readability
+
+
+    def all_pdf_tables(self):
+        with open("all_pdf_tables.txt", "a") as table_file:
+            for pdf_location in self.full_file_paths:
+                # Attempt to read tables from the current PDF
+                tables = tabula.read_pdf(pdf_location, pages="all", multiple_tables=True)
+
+                # Iterate over each table (DataFrame) found
+                for table in tables:
+                    # Convert the DataFrame to CSV format and write it to the file
+                    table_file.write(table.to_csv(index=False))
+                    table_file.write("\n\n")  # Add spacing for readability between tables
+
+            
+
     def pdf_to_text(self):
         with open("pdf_text.txt", "a") as pdf_pages:  # Open the output file once, outside the loop
             for pdf in self.full_file_paths:
@@ -45,11 +74,8 @@ class PdfOrganizer:
                         text = page_obj.extractText()
                         pdf_pages.write(text)  # Write the extracted text to the file
 
-                
-
-
 
 # Usage
 banking = os.path.expanduser("~/Documents/e_banking")
 my_files = PdfOrganizer(banking)
-my_files.pdf_to_text()
+my_files.all_pdf_tables()
